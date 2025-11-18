535638d350c0de6af012fdfd13eaad19e13a1705
Haylie Fleming <hfleming989@gmail.com>
2025-11-17 18:56:03 -0600

    edited App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,38 +8,35 @@ import History from "./Pages/History";
 import "./index.css";
 
 function App() {
+
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/workout/:id" element={<WorkoutDetails />} />
-      </Routes>
+        {/* Side menu */}
+      <div className={`side-menu ${menuOpen ? "open" : ""}`}>
+        {menuOpen && (
+          <>
+            <Link to="/" onClick={() => setMenuOpen(false)}>Landing</Link>
+            <Link to="/planning" onClick={() => setMenuOpen(false)}>Planning</Link>
+            <Link to="/workout" onClick={() => setMenuOpen(false)}>Workout</Link>
+            <Link to="/history" onClick={() => setMenuOpen(false)}>History</Link>
+          </>
+        )}
+      </div>
+
+      {/* Overlay */}
+      {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
+    
+      {/* Page content */}
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/planning" element={<Planning />} />
+          <Route path="/workout" element={<Workout />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
-
-    {/* Side menu */}
-    <div className={`side-menu ${menuOpen ? "open" : ""}`}>
-      {menuOpen && (
-        <>
-          <Link to="/" onClick={() => setMenuOpen(false)}>Landing</Link>
-          <Link to="/planning" onClick={() => setMenuOpen(false)}>Planning</Link>
-          <Link to="/workout" onClick={() => setMenuOpen(false)}>Workout</Link>
-          <Link to="/history" onClick={() => setMenuOpen(false)}>History</Link>
-        </>
-      )}
-    </div>
-
-    {/* Overlay */}
-    {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
-  
-    {/* Page content */}
-    <div className="page-content">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/planning" element={<Planning />} />
-        <Route path="/workout" element={<Workout />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
-    </div>
 
